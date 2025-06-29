8bda18f99a5b4e6e7ea39cc49c41d4cb29433517
Matt Balmer <signup@mattbalmer.com>
2025-06-29 18:28:44 -0400

    Test column move

--- src/server/knbn.ts
+++ src/server/knbn.ts
@@ -1,5 +1,5 @@
 // TODO: Figure out better import/export paths
-export { loadBoard, updateTaskInBoard, saveBoard } from 'knbn/src/core/boardUtils';
+export * from 'knbn/src/core/boardUtils';
 import { version, boardVersion } from 'knbn/package.json';
 
 export const KNBN_CORE_VERSION = version;
